a7f42d58963323779dd91eaac44b7ffd27ddd100
liulei <satbirdd@gmail.com>
2015-10-23 09:49:08 +0800

    devide collection and member routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :tempaltes
+  resources :tempaltes, only: [:index, :new, :create]
 
-  resources :tempaltes, path: 'tempaltes/blob'
+  resources :tempaltes, only: [:show, :update, :edit, :destroy], path: 'tempaltes/blob'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
